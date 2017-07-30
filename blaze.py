0332558e458c4ba6ae47c7f8a4c5797751114e92
arunpatro <arun.kumar.patro@gmail.com>
2017-07-30 17:52:14 +0530

    plot with many

--- blaze.py
+++ blaze.py
@@ -2,8 +2,9 @@ from multiprocessing import Pool
 import requests
 import numpy as np
 import time
-url = 'http://releases.ubuntu.com/16.04.2/SHA1SUMS'
-# url = 'http://releases.ubuntu.com/16.04.2/ubuntu-16.04.2-server-amd64.template'
+from matplotlib import pyplot as plt
+# url = 'http://releases.ubuntu.com/16.04.2/SHA1SUMS'
+url = 'http://releases.ubuntu.com/16.04.2/ubuntu-16.04.2-desktop-amd64.iso.zsync'
 
 proxies = {
   'http': 'http://10.3.100.207:8080',
@@ -18,30 +19,47 @@ def getRanges(bytesRange, n):
 
 def unit(tup):
     headers={'Range': 'bytes={}-{}'.format(tup[1][0], tup[1][1])}
-    r = requests.get(url, proxies=proxies, headers=headers)
-    print 'Completed connection {}'.format(tup[0])
+    while True:
+        try:
+            r = requests.get(url, proxies=proxies, headers=headers)
+            break
+        except Exception as e:
+            continue
+    # print 'Completed connection {}'.format(tup[0])
     return r.content
 
-def verify(hash1, hash2):
-    return hash1==hash2
     # print hashlib.md5(open(full_path, 'rb').read()).hexdigest()
 
-
 if __name__ == '__main__':
-    n = 10
-    p = Pool(n)
-    
-    response = requests.head(url)
-    length = int(response.headers.get('content-length'))
-    ranges = getRanges((0,length-1), n)
-    t  = time.time()
-    ranges =  list(enumerate(ranges))
-    out = p.map(unit, ranges)
-    print time.time() - t
-    out = reduce((lambda x, y: x + y), out)
-    truth = requests.get(url, proxies=proxies).content
-    print verify(out, truth)
+    s = [1, 2 ,5, 10, 20, 50, 100, 200, 500]
+    ts = []
+    for i in s:
+        n = 10
+        p = Pool(i)
+        
+        response = requests.head(url)
+        length = int(response.headers.get('content-length'))
+        ranges = getRanges((0,length-1), n)
+        ranges =  list(enumerate(ranges))
+        t  = time.time()
+        out = p.map(unit, ranges)
+        ts.append( time.time() - t )
+        print i
 
+    plt.figure()
+    plt.plot(s, ts)
+    plt.savefig('blaze.png')
+    plt.show()
+
+    out = reduce((lambda x, y: x + y), out)
+    # truth = requests.get(url, proxies=proxies).content
+    # print out == truth
+
+
+    # r2 = requests.get(url, proxies=proxies, stream=True)
+    # print list(r2.iter_content(chunk_size=length/n))
+    # print len(list(r2.iter_content(chunk_size=length/n)))
+
+    print 'Saving'
     with open(url.split('/')[-1], 'w') as f:
         f.write(out)
-
